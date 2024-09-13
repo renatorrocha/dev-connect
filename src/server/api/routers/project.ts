a03b66cf1311e2ad7b9fab16bf5df2103c1b1eb2
renatorrodrigues <renatorrodrigues2002@gmail.com>
2024-09-12 21:18:36 -0300

    feat(api): project.getAll

---
+++ src/server/api/routers/project.ts
@@ -0,0 +1,26 @@
+import { z } from "zod";
+
+import {
+  createTRPCRouter,
+  protectedProcedure,
+  publicProcedure,
+} from "~/server/api/trpc";
+
+export const projectRouter = createTRPCRouter({
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const projects = await ctx.db.project.findMany();
+
+    return projects ?? null;
+  }),
+
+  // create: protectedProcedure
+  //   .input(z.object({ name: z.string().min(1) }))
+  //   .mutation(async ({ ctx, input }) => {
+  //     return ctx.db.post.create({
+  //       data: {
+  //         name: input.name,
+  //         createdBy: { connect: { id: ctx.session.user.id } },
+  //       },
+  //     });
+  //   }),
+});
