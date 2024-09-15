3a338717fbdab70bda1397932cb7eacb743bcf5f
renatorrodrigues <renatorrodrigues2002@gmail.com>
2024-09-15 20:08:42 -0300

    fix: auth config

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -10,48 +10,24 @@ import Github from "next-auth/providers/github";
 import { env } from "~/env";
 import { db } from "~/server/db";
 
-/**
- * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
- * object and keep type safety.
- *
- * @see https://next-auth.js.org/getting-started/typescript#module-augmentation
- */
 declare module "next-auth" {
   interface Session extends DefaultSession {
     user: {
       id: string;
-      username: string;
       // ...other properties
       // role: UserRole;
     } & DefaultSession["user"];
   }
-
-  // interface User {
-  //   // ...other properties
-  //   // role: UserRole;
-  // }
 }
 
-/**
- * Options for NextAuth.js used to configure adapters, providers, callbacks, etc.
- *
- * @see https://next-auth.js.org/configuration/options
- */
 export const authOptions: NextAuthOptions = {
   callbacks: {
-    jwt: async ({ token, profile, user }) => {
-      if (user) token.user = user;
-      if (profile) token.profile = profile;
-
-      return token;
-    },
-    session: async ({ session, user, token }) => {
+    session: async ({ session, user }) => {
       return {
         ...session,
         user: {
           ...session.user,
-          id: user?.id,
-          username: (token.profile as { login?: string })?.login,
+          id: user.id,
         },
       };
     },
@@ -61,38 +37,13 @@ export const authOptions: NextAuthOptions = {
     signIn: "/",
     signOut: "/",
   },
-  session: { strategy: "jwt" },
   secret: env.NEXTAUTH_SECRET,
   providers: [
     Github({
       clientId: env.GITHUB_CLIENT_ID,
       clientSecret: env.GITHUB_CLIENT_SECRET,
-      // profile(profile) {
-      //   return {
-      //     id: profile.id.toString(),
-      //     name: profile.name || profile.login,
-      //     email: profile.email,
-      //     image: profile.avatar_url,
-      //     username: profile.login,
-      //   } as NextAuthUserWithStringId;
-      // },
     }),
-
-    /**
-     * ...add more providers here.
-     *
-     * Most other providers require a bit more work than the Discord provider. For example, the
-     * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
-     * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
-     *
-     * @see https://next-auth.js.org/providers/github
-     */
   ],
 };
 
-/**
- * Wrapper for `getServerSession` so that you don't need to import the `authOptions` in every file.
- *
- * @see https://next-auth.js.org/configuration/nextjs
- */
 export const getServerAuthSession = () => getServerSession(authOptions);
