14e26f66853f140f223a5e0b5d0b86d5c61a8962
renatorrodrigues <renatorrodrigues2002@gmail.com>
2024-09-15 21:14:55 -0300

    feat: project card component

--- src/app/your-projects/page.tsx
+++ src/app/your-projects/page.tsx
@@ -5,21 +5,15 @@ import React from "react";
 import { buttonVariants } from "~/components/ui/button";
 import Link from "next/link";
 import { api } from "~/trpc/react";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "~/components/ui/card";
-import { CalendarDaysIcon } from "lucide-react";
+import ProjectCard from "~/components/project-card";
+import { Loader2 } from "lucide-react";
 
 export default function YourProjects() {
   const { data } = useSession();
-  const { data: projectsArray } = api.project.getAllProjectsByUserId.useQuery({
-    userId: data?.user.id,
-  });
+  const { data: projectsArray, isLoading } =
+    api.project.getAllProjectsByUserId.useQuery({
+      userId: data?.user.id ?? "",
+    });
 
   return (
     <div className="mx-auto w-full max-w-6xl px-4 py-12 md:px-6">
@@ -35,41 +29,15 @@ export default function YourProjects() {
           Create Project
         </Link>
       </div>
-      <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
-        {projectsArray?.map((project) => (
-          <Card key={project.id}>
-            {/* //todo: implementar foto do projeto  */}
-            {/* <CardContent className="p-0">
-              <img
-                src="/placeholder.svg"
-                alt="Project Thumbnail"
-                width={400}
-                height={225}
-                className="aspect-video w-full object-cover"
-              />
-            </CardContent> */}
-
-            <CardHeader className="p-4">
-              <CardTitle>{project.name}</CardTitle>
-              <CardDescription>{project.description}</CardDescription>
-            </CardHeader>
-
-            <CardFooter className="flex items-center justify-between p-4">
-              <Link
-                href={`/projects/${project.id}`}
-                className={buttonVariants()}
-                prefetch={false}
-              >
-                View Project
-              </Link>
-              <div className="flex items-center gap-2 text-sm text-muted-foreground">
-                <CalendarDaysIcon className="size-4" />
-                <p>{new Date(project.createdAt).toLocaleDateString()}</p>
-              </div>
-            </CardFooter>
-          </Card>
-        ))}
-      </div>
+      {isLoading ? (
+        <Loader2 className="m-auto mt-32 size-8 animate-spin text-primary" />
+      ) : (
+        <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+          {projectsArray?.map((project) => (
+            <ProjectCard project={project} key={project.id} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/components/project-card.tsx
@@ -0,0 +1,53 @@
+import {
+  Card,
+  // CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
+import { CalendarDaysIcon } from "lucide-react";
+import { formatDistanceToNow } from "date-fns";
+import type { Project } from "@prisma/client";
+import Link from "next/link";
+import { buttonVariants } from "./ui/button";
+
+export default function ProjectCard({ project }: { project: Project }) {
+  const createdDateRelativeToNow = formatDistanceToNow(project.createdAt, {
+    addSuffix: true,
+  });
+
+  return (
+    <Card key={project.id}>
+      {/* //todo: implementar foto do projeto  */}
+      {/* <CardContent className="p-0">
+      <img
+        src="/placeholder.svg"
+        alt="Project Thumbnail"
+        width={400}
+        height={225}
+        className="aspect-video w-full object-cover"
+      />
+    </CardContent> */}
+
+      <CardHeader className="p-4">
+        <CardTitle>{project.name}</CardTitle>
+        <CardDescription>{project.description}</CardDescription>
+      </CardHeader>
+
+      <CardFooter className="flex items-center justify-between p-4">
+        <Link
+          href={`/projects/${project.id}`}
+          className={buttonVariants()}
+          prefetch={false}
+        >
+          View Project
+        </Link>
+        <div className="flex items-center gap-2 text-sm text-muted-foreground">
+          <CalendarDaysIcon className="size-4" />
+          <p>{createdDateRelativeToNow}</p>
+        </div>
+      </CardFooter>
+    </Card>
+  );
+}
