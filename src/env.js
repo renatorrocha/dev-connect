36bc47ba32be169c0d042b96bc4109d6478c4c52
renatorrodrigues <renatorrodrigues2002@gmail.com>
2024-09-16 23:09:12 -0300

    build: rename github variables to App_github

--- src/env.js
+++ src/env.js
@@ -22,8 +22,8 @@ export const env = createEnv({
       // VERCEL_URL doesn't include `https` so it cant be validated as a URL
       process.env.VERCEL ? z.string() : z.string().url(),
     ),
-    GITHUB_CLIENT_ID: z.string(),
-    GITHUB_CLIENT_SECRET: z.string(),
+    APP_GITHUB_CLIENT_ID: z.string(),
+    APP_GITHUB_CLIENT_SECRET: z.string(),
   },
 
   /**
@@ -44,8 +44,8 @@ export const env = createEnv({
     NODE_ENV: process.env.NODE_ENV,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-    GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
-    GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
+    APP_GITHUB_CLIENT_ID: process.env.APP_GITHUB_CLIENT_ID,
+    APP_GITHUB_CLIENT_SECRET: process.env.APP_GITHUB_CLIENT_ID,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
