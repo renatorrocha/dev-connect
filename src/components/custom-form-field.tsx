e7f06c55eb683f1dbd24681615d76e82e52998cc
renatorrocha <renatorrodrigues2002@gmail.com>
2024-09-29 16:41:06 -0300

    build: fix errors

--- src/components/custom-form-field.tsx
+++ src/components/custom-form-field.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import type { Control, ControllerRenderProps } from "react-hook-form";
 import {
   FormControl,
