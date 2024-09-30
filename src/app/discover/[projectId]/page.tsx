6f4913a20e2a141d1652c5234857ebf1002b82e6
renatorrocha <renatorrodrigues2002@gmail.com>
2024-09-30 17:00:48 -0300

    feat: discover page and projectId page

---
+++ src/app/discover/[projectId]/page.tsx
@@ -0,0 +1,101 @@
+"use client";
+
+import { api } from "~/trpc/react";
+import MarkdownPreview from "@uiw/react-markdown-preview";
+import rehypeSanitize from "rehype-sanitize";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
+import { buttonVariants } from "~/components/ui/button";
+import { CodeIcon, GitBranchIcon, Loader2 } from "lucide-react";
+import Link from "next/link";
+import { Badge } from "~/components/ui/badge";
+
+export default function DiscoverProjectIdPage({
+  params: { projectId },
+}: SearchParamProps) {
+  if (!projectId) return null;
+  const rehypePlugins = [rehypeSanitize];
+
+  const {
+    data: project,
+    isLoading,
+    isError,
+  } = api.project.getById.useQuery({
+    projectId,
+  });
+
+  if (isLoading) {
+    return (
+      <Loader2 className="m-auto mt-32 size-8 animate-spin text-primary" />
+    );
+  }
+
+  if (isError) {
+    return (
+      <div className="mx-auto mt-32 w-fit rounded-xl border bg-card p-8 text-center text-xl text-destructive shadow">
+        Project not found.
+      </div>
+    );
+  }
+  console.log(project);
+  return (
+    project && (
+      <div className="container mt-10 max-w-6xl px-8">
+        <Card className="mb-8">
+          <CardHeader className="flex flex-col space-y-4 sm:flex-row sm:items-start sm:justify-between sm:space-y-0">
+            <div>
+              <CardTitle className="text-2xl font-bold capitalize text-primary">
+                {project.name}
+              </CardTitle>
+              <CardDescription className="mt-2">
+                {project.description}
+              </CardDescription>
+            </div>
+            <Link
+              href={project.repositoryLink}
+              target="_blank"
+              rel="noopener noreferrer"
+              className={buttonVariants({ size: "sm" })}
+            >
+              <GitBranchIcon className="mr-2 h-4 w-4" />
+              View Repository
+            </Link>
+          </CardHeader>
+        </Card>
+
+        <Card>
+          <CardHeader className="border-b">
+            <CardTitle className="flex w-full items-center justify-between">
+              <p className="text-xl font-semibold text-primary">Readme</p>
+
+              <div className="gap-2">
+                <Badge variant="secondary" className="lowercase">
+                  <CodeIcon className="mr-2 h-3 w-3" />
+                  {project.techStack}
+                </Badge>
+              </div>
+            </CardTitle>
+          </CardHeader>
+
+          <CardContent className="p-2">
+            <div className="max-w-none">
+              <MarkdownPreview
+                source={project.readme}
+                style={{ padding: "2rem" }}
+                rehypePlugins={rehypePlugins}
+                wrapperElement={{
+                  "data-color-mode": "light",
+                }}
+              />
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    )
+  );
+}
--- src/app/discover/page.tsx
+++ src/app/discover/page.tsx
@@ -1,5 +1,64 @@
+"use client";
+
+import { Loader2 } from "lucide-react";
 import React from "react";
+import PreviewProjectCard from "~/components/preview-project-card";
+import { api } from "~/trpc/react";
 
 export default function DiscoverPage() {
-  return <div>DiscoverPage</div>;
+  const { data: projects, isLoading } = api.project.getAll.useQuery();
+
+  return (
+    <div className="container mt-10 max-w-6xl px-8">
+      <div className="mb-8">
+        <h1 className="text-3xl font-bold text-primary">
+          Discover New Projects
+        </h1>
+
+        <p className="text-muted-foreground">Discover exciting projects.</p>
+      </div>
+
+      {/* <div className="mb-8 grid gap-6 md:grid-cols-2">
+      <div>
+          <Label htmlFor="name-filter">Filter by Name</Label>
+          <Input
+            id="name-filter"
+            placeholder="Enter project name"
+            value={nameFilter}
+            onChange={(e) => setNameFilter(e.target.value)}
+          />
+        </div>
+        <div>
+          <Label htmlFor="tech-stack-filter">Filter by Tech Stack</Label>
+          <Select onValueChange={setTechStackFilter} value={techStackFilter}>
+            <SelectTrigger id="tech-stack-filter">
+              <SelectValue placeholder="Select Tech Stack" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="">All</SelectItem>
+              <SelectItem value="frontend">Frontend</SelectItem>
+              <SelectItem value="backend">Backend</SelectItem>
+              <SelectItem value="fullstack">Full Stack</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+      </div> */}
+
+      {isLoading ? (
+        <Loader2 className="m-auto mt-32 size-8 animate-spin text-primary" />
+      ) : (
+        <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+          {projects?.map((project) => (
+            <PreviewProjectCard key={project.id} project={project} />
+          ))}
+        </div>
+      )}
+
+      {projects?.length === 0 && (
+        <p className="mt-8 text-center text-gray-500">
+          No projects found matching the current filters.
+        </p>
+      )}
+    </div>
+  );
 }
