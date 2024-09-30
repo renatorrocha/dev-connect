1ffc18f6f481b5224e4e6a0ac8b1c9d44e35c80c
renatorrocha <renatorrodrigues2002@gmail.com>
2024-09-30 15:20:03 -0300

    fix: add animations and techstack in preview card

--- src/components/preview-project-card.tsx
+++ src/components/preview-project-card.tsx
@@ -6,11 +6,12 @@ import {
   CardHeader,
   CardTitle,
 } from "~/components/ui/card";
-import { CalendarDaysIcon } from "lucide-react";
+import { CalendarDaysIcon, CodeIcon } from "lucide-react";
 import { formatDistanceToNow } from "date-fns";
 import type { Project } from "@prisma/client";
 import { buttonVariants } from "./ui/button";
-import Link from 'next/link';
+import Link from "next/link";
+import { Badge } from "./ui/badge";
 
 export default function PreviewProjectCard({ project }: { project: Project }) {
   const createdDateRelativeToNow = formatDistanceToNow(project.createdAt, {
@@ -18,7 +19,10 @@ export default function PreviewProjectCard({ project }: { project: Project }) {
   });
 
   return (
-    <Card key={project.id}>
+    <Card
+      key={project.id}
+      className="shadow-md transition-all duration-300 hover:scale-105 hover:shadow-lg"
+    >
       {/* //todo: add project img  */}
       {/* <CardContent className="p-0">
       <img
@@ -31,8 +35,19 @@ export default function PreviewProjectCard({ project }: { project: Project }) {
     </CardContent> */}
 
       <CardHeader className="p-4">
-        <CardTitle>{project.name}</CardTitle>
-        <CardDescription>{project.description}</CardDescription>
+        <CardTitle className="flex items-center justify-between">
+          <p className="w-36 truncate">{project.name}</p>
+
+          <div className="gap-2">
+            <Badge variant="secondary" className="lowercase">
+              <CodeIcon className="mr-2 h-4 w-4" />
+              {project.techStack}
+            </Badge>
+          </div>
+        </CardTitle>
+        <CardDescription className="line-clamp-2 h-10">
+          {project.description}
+        </CardDescription>
       </CardHeader>
 
       <CardFooter className="flex items-center justify-between p-4">
