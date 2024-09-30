f7766e4e64630ee810b53f7b7477714172ecfd88
renatorrocha <renatorrodrigues2002@gmail.com>
2024-09-30 13:58:23 -0300

    feat: project logo

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,13 +2,30 @@ import Link from "next/link";
 import { buttonVariants } from "~/components/ui/button";
 import AuthButton from "./auth-btn";
 import { getServerAuthSession } from "~/server/auth";
+import Image from "next/image";
 
 export default async function Header() {
   const session = await getServerAuthSession();
 
   return (
     <header className="flex items-center justify-between">
-      <Link href={"/"}>Logo</Link>
+      <Link
+        href={"/"}
+        className="flex items-center gap-4 transition-all duration-300 hover:translate-x-2"
+      >
+        <Image
+          src={"/logo_icon.svg"}
+          alt=""
+          width={100}
+          height={100}
+          className="size-10"
+        />
+
+        <p className="text-lg font-medium">
+          Dev
+          <span className="gradient-primary-secondary font-bold">.Connect</span>
+        </p>
+      </Link>
 
       <nav className="flex items-center gap-5">
         <Link href="/discover" className={buttonVariants({ variant: "link" })}>
